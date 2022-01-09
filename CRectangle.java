f61a6797ee4f5cc91f376dcb0dd2bc0637695bf3
lightraineiei <91459035+lightraineiei@users.noreply.github.com>
2022-01-09 16:55:14 +0800

    Add files via upload

---
+++ CRectangle.java
@@ -0,0 +1,9 @@
+// CRectangle.java, 此檔案置於pack6\subpack2資料夾內
+package pack6.subpack2;    // 將CRectangle類別納入pack6.subpack2中
+public class CRectangle
+{
+   public void show()
+   {
+      System.out.println("show() in class CRectangle called");
+   }
+}
