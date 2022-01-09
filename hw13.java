5956d671b1d7cad0820127a032a8dd37f43979c9
lightraineiei <91459035+lightraineiei@users.noreply.github.com>
2022-01-09 18:12:32 +0800

    Add files via upload

---
+++ hw13.java
@@ -0,0 +1,12 @@
+
+public class hw13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  str = "Habit is sceond nature.";
+		System.out.println("stung="+str);
+		System.out.println("sub-string="+str.substring(16,22));
+		
+	}
+
+}
